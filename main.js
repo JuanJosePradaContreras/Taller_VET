d73c6907b36c5de64b6b2eb2e41c8e46029a037b
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2025-03-27 11:46:07 -0500

    feat: :sparkles: class add

---
+++ main.js
@@ -0,0 +1,19 @@
+class Mascota {
+    constructor(nombre, especie, edad, peso, estadoSalud) {
+      this.nombre = nombre;
+      this.especie = especie;
+      this.edad = edad;
+      this.peso = peso;
+      this.estadoSalud = estadoSalud;
+    }
+  }
+  
+  const mascotas = [];
+  
+  function registrarMascota() {
+    let nombre = prompt("Ingrese el nombre de la mascota:");
+    let especie = prompt("Ingrese la especie de la mascota (Perro, Gato, etc.):");
+    let edad = prompt("Ingrese la edad de la mascota:");
+    let peso = prompt("Ingrese el peso de la mascota:");
+    let estadoSalud = prompt("Ingrese el estado de salud (Sano, Enfermo, En tratamiento):");
+  }
